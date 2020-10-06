95c0197144da270c6e0bf3a0d54189e3c5468f4d
Renata Caetano <renatapcaetano@hotmail.com>
2020-10-06 20:50:46 -0300

    exercicios-planner

---
+++ semana4/planner/index.js
@@ -0,0 +1,38 @@
+/*function adicionaTarefa(){
+const minhaTarefa = document.getElementById("tarefa").value;
+   
+    if (minhaTarefa.value !== "") { //SE O VALOR DO INPUT FOR DIFERENTE DE VAZIO ELE VAI P LISTA E RETORNA VAZIO
+        const minhaTarefa = document.getElementById("tarefa")
+        minhaTarefa.value=""};
+
+const diaSemana = document.getElementById("dias-semana");
+
+switch(diaSemana.value){
+case "domingo":
+    document.getElementById("domingo").innerHTML +=`<p>${minhaTarefa}</p>`;
+    
+    break;
+case "segunda":
+    document.getElementById("segunda").innerHTML +=`<p>${minhaTarefa}</p>`;
+    break;
+case "terca":
+        document.getElementById("terca").innerHTML +=`<p>${minhaTarefa}</p>`;
+    break;
+case "quarta":
+    document.getElementById("quarta").innerHTML +=`<p>${minhaTarefa}</p>`;
+    break;
+
+case "quinta":
+    document.getElementById("quinta").innerHTML +=`<p>${minhaTarefa}</p>`;
+    break;
+
+case "sexta":
+    document.getElementById("sexta").innerHTML +=`<p>${minhaTarefa}</p>`;
+    break;
+case "sabado":
+    document.getElementById("sabado").innerHTML +=`<p>${minhaTarefa}</p>`;
+    break;
+
+}
+
+}*/
