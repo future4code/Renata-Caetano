bff008faa828ca407c831a8607d4303a1f4ed826
Renata Caetano <renatapcaetano@hotmail.com>
2020-10-09 12:06:02 -0300

    exercicios-de-escrita

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,21 @@
+//EXERCICIOS DE ESCRITA
+//01- Tem uma função para converter valor de dólar em real solicitando ao usuário a cotação do dia.Escrevendo na tela o resultado de 100 vezes a cotação digitada, mostrando o valor em real.
+
+// 02-Tenho uma função que se chama investe dinheiro e nela existem tipos de parametros,que é tipo de
+// investimento e o valor, e tem uma variavel que guarda valor apos investimento.
+// existes casos para escolher o tipo de investimento e se ele for escolhido 
+// possui a fórmula do que é para ser feito. e retorna o valor para a variavel responsável valorAposInvestimento.
+// NovoMontante executa a função com valor 150 em ações e o segundo montante executa o alerta pois não há o tipo de investimento no case.
+
+// 03 - Possui 03 array, uma com valores e duas sem valores.
+// Para  o numero do array se ele for dividido por 2 com resto 0 guarde ele no array1 senão guarde no array2
+// Escreva "Quantidade total de numeros , tamanho do array numeros"
+// Escreva tamanho do array1
+// Escreva o tamanho do array2 
+
+// //04 - Temos 1 array com valores e duas variaveis que se chamam numero1 recebendo valor infinito
+// e outra que se chama numero2 recebendo o valor 0.
+// no laço ele ira chamar o numero do array numeros e se o numero for menor que o infinito ele irá escrever 
+// o valor no console.
+//outro caso é se este valor do array numeros for maior que 0 ele irá escrever este valor.
+// Observação no caso 02 ele só não irá escrever o numero -10.
