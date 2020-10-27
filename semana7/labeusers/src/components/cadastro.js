42fbd4fb4b3e6c1b8f83956ae9d89c6fbe5533cf
Renata Caetano <renatapcaetano@hotmail.com>
2020-10-27 17:04:14 -0300

    exercicio-labeusers

---
+++ semana7/labeusers/src/components/cadastro.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import axios from 'axios'
+import Registros from './registros'
+
+
+export default class Cadastro extends React.Component{
+render(){
+return(
+<div>
+
+<h1>Cadastro</h1>
+
+<p>Nome:</p>
+<input></input>
+
+<p>Email:</p>
+<input></input>
+
+<button>Salvar</button>
+
+</div>
+
+      )
+    }
+}
+
+
+  
+
+
+      
---
+++ semana7/labeusers/src/components/registros.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import axios from 'axios';
+
+
+export default class Registros extends React.Component{
+    render(){
+      return (
+    
+<div>
+
+<h1>Lista de Registros</h1>
+
+</div>
+
+      );}}
