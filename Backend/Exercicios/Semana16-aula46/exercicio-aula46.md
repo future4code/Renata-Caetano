1c542c3040db632209bfdf0337c74d45688b6d33
Renata Caetano <renatapcaetano@hotmail.com>
2021-01-13 17:00:46 -0300

    iniciando-exercicios

---
+++ Backend/Exercicios/Semana16-aula46/exercicio-aula46.md
@@ -0,0 +1,5 @@
+### Exercicio 01
+
+A- Recebemos resultados da query e outras informações.
+
+B- 
---
+++ Backend/Exercicios/Semana16-aula46/src/index.ts
@@ -0,0 +1,71 @@
+import knex from "knex";
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import { AddressInfo } from "net";
+
+
+/**************************************************************/
+
+dotenv.config();
+
+/**************************************************************/
+
+const connection = knex({   
+  client: "mysql",
+  connection: {
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+});
+
+/**************************************************************/
+
+const app = express();
+
+app.use(express.json());
+
+const server = app.listen(process.env.PORT || 3003, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Server is running in http://localhost:${address.port}`);
+  } else {
+    console.error(`Failure upon starting server.`);
+  }
+});
+
+/**************************************************************/
+
+app.get('/', testEndpoint)
+
+async function testEndpoint(req:Request, res:Response): Promise<void>{
+  try {
+    const result = await connection.raw(`
+      SELECT * FROM Actor
+    `)
+
+    res.status(200).send(result)
+  } catch (error) {
+    res.status(400).send(error.message)
+  }
+}
+
+
+// const searchActor = async (name: string): Promise<any> => {
+//   const result = await connection.raw(`
+//     SELECT * FROM Actor WHERE name = "Amanda Caetano"
+//     `)
+//   return result 
+// }
+
+const countActors = async (gender: string): Promise<any> => {
+  const result = await connection.raw(`
+    SELECT COUNT(*) as count FROM Actor WHERE gender = "${gender}"
+  `);
+	// Só colocamos esse "as count" como apelido, para ficar mais fácil de pegar
+	// o valor no resultado!
+  const count = result[0][0].count;
+  return count;
+};
