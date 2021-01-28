d6d4f85cc875f225f2845c24113c1bde0e348063
Renata Caetano <renatapcaetano@hotmail.com>
2021-01-28 16:18:21 -0300

    exercicio-1-erro-localhost

---
+++ Backend/Exercicios/semana18-aula53/src/readme.md
@@ -0,0 +1,4 @@
+### Estruturando projeto Cookenu
+
+1-Cadastro
+ o id, e-mail, nome a sua senha para realizar o cadastro. A senha tem uma regra: ela deve conter, no mínimo, 6 carácteres. 
---
+++ Backend/Exercicios/semana18-aula53/src/teste.sql
@@ -0,0 +1,10 @@
+CREATE TABLE create_user_cookenu (
+	id VARCHAR (255) PRIMARY KEY, 
+    name VARCHAR(255) NULL, 
+    email VARCHAR(255) NOT NULL UNIQUE ,
+    password VARCHAR(10)  NOT NULL 
+    );
+
+    DROP TABLE create_user_cookenu;
+
+    SELECT * FROM create_user_cookenu;
